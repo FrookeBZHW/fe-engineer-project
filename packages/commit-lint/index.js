2c2265487701b0dfc71e49a396d51ef0c3b91a38
MoBenShiWu <qiumeihui@aliyun.com>
2023-09-19 22:47:49 +0800

    feat: 添加commit校验

--- packages/commit-lint/index.js
+++ packages/commit-lint/index.js
@@ -1,6 +1,6 @@
-module.export = {
-    parsePreset:'conventional-changelog-conventionalcommits',
-    rules:{
-        'type-enum':[2,'always',['feat','fix']]
-    }
-}
+module.exports = {
+    parserPreset: 'conventional-changelog-conventionalcommits',
+    rules: {
+        'type-enum': [2, 'always', ['feat', 'fix', 'docs', 'style', 'test', 'refactor', 'chore', 'revert']],
+    },
+};
