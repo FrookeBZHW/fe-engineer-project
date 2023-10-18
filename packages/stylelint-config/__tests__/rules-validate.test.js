87ec63f9c2a907c0742a9f43eece596014637fa3
MoBenShiWu <qiumeihui@aliyun.com>
2023-10-18 15:56:11 +0800

    feat: stylelint集成

---
+++ packages/stylelint-config/__tests__/rules-validate.test.js
@@ -0,0 +1,57 @@
+const assert = require('assert');
+const stylelint = require('stylelint');
+const path = require('path')
+
+console.log('用户包管理', process.env.npm_config_user_agent);
+
+describe('test/rules-validate.test.js', () => {
+    it('Validate css', async () => {
+        // 找到待测试css文件
+        const filePaths = [path.join(__dirname, './fixtures/index.css')];
+        const result = await stylelint.lint({
+            configFile: path.join(__dirname, '../index.js'),
+            files: filePaths,
+            fix: false, // 开启为一键修复可自动修复的内容
+        });
+
+        console.log(result);
+
+        // defaultSeverity warning时errored为false
+        if (result?.errored) {
+            // 如果有错 查看output中警告内容
+            const filesResult = JSON.parse(result.output || '[]') || [];
+            console.log('filesResult===');
+            console.log(filesResult);
+
+            filesResult.forEach(fileResult => {
+                console.log(`========= ${filePaths} ==========`);
+                console.log(fileResult.warnings);
+            })
+            // > 0 表示符合预期
+            assert.ok(filesResult.length > 0);
+        }
+
+    })
+
+    it('Validate scss', async () => {
+        const filePaths = [path.join(__dirname, './fixtures/index.scss')];
+        const result = await stylelint.lint({
+            configFile: path.join(__dirname, '../index.js'),
+            files: filePaths,
+            fix: false
+        })
+        console.log('scss============');
+        console.log(result);
+
+        // defaultSeverity warning时errored为false
+        if (result?.errored) {
+            const filesResult = JSON.parse(result.output || '[]') || [];
+            console.log('scss filesResult===');
+            console.log(filesResult);
+
+            filesResult.forEach(fileResult => {
+                console.log(fileResult.warnings);
+            })
+        }
+    })
+})
