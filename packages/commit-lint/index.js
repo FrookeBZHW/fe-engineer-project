486eafdb77f3ca2c4c38759e195ec9b40f55584c
MoBenShiWu <qiumeihui@aliyun.com>
2023-10-29 00:36:46 +0800

    chore: 加入release命令

--- packages/commit-lint/index.js
+++ packages/commit-lint/index.js
@@ -2,10 +2,11 @@
  * @Author: MoBenShiWu
  * @Date: 2023-09-19 22:43:00
  * @LastEditors: MoBenShiWu
- * @LastEditTime: 2023-10-28 23:31:26
+ * @LastEditTime: 2023-10-28 23:55:12
  * @FilePath: /fe-engineer/lerna-demo/packages/commit-lint/index.js
  */
 module.exports = {
+    // 配置用于解析提交消息的preset
     parserPreset: 'conventional-changelog-conventionalcommits',
     rules: {
         'type-enum': [2, 'always', ['feat', 'fix', 'docs', 'style', 'test', 'refactor', 'chore']],
